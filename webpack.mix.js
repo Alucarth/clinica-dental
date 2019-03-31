cc8abdd157e2df06a3819f56f3f14ebd4e5be9f3
alucarth <ltorrezs2008@gmail.com>
2019-03-31 00:58:03 -0400

    modificando git ignore app css public

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+   .less('resources/less/adminlte.less', 'public/css')
    .sass('resources/sass/app.scss', 'public/css');
